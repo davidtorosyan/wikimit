1243fc40edba8c1679faaa34b6e4d0ef93529bff
David Torosyan <davidtorosyan.git@gmail.com>
2023-12-16 12:17:24 -0800

    Switch to using the responses mock

--- src/wikimit-engine/tests/unit/test_wiki.py
+++ src/wikimit-engine/tests/unit/test_wiki.py
@@ -1,7 +1,21 @@
+import responses
 from functions.revision_sync import wiki
+from responses import _recorder
+
+MOCK_TITLE = "Finch"
+MOCK_SMOKE_CURRENT_PATH = "tests/mock/wiki_smoke_current.yaml"
 
 
+@responses.activate
 def test_current_page_info():
-    info = wiki.current_page_info("Finch")
+    responses._add_from_file(file_path=MOCK_SMOKE_CURRENT_PATH)
+    info = wiki.current_page_info(MOCK_TITLE)
+    assert info.title == MOCK_TITLE
 
-    assert info.title == "Finch"
+
+@_recorder.record(file_path=MOCK_SMOKE_CURRENT_PATH)
+def record_smoke_current():
+    """
+    Run this function to record the responses for the smoke test.
+    """
+    wiki.current_page_info(MOCK_TITLE)
