19f26c74b05fd0161db30e40abd02da1e609d960
David Torosyan <davidtorosyan.git@gmail.com>
2023-12-16 12:19:20 -0800

    Add asserts

--- src/wikimit-engine/tests/unit/test_wiki.py
+++ src/wikimit-engine/tests/unit/test_wiki.py
@@ -11,6 +11,10 @@ def test_current_page_info():
     responses._add_from_file(file_path=MOCK_SMOKE_CURRENT_PATH)
     info = wiki.current_page_info(MOCK_TITLE)
     assert info.title == MOCK_TITLE
+    assert info.url == "https://en.wikipedia.org/wiki/Finch"
+    assert info.language == "en"
+    assert info.highest_known_revision_id == "1186000588"
+    assert info.highest_known_revision_timestamp == "2023-11-20T08:31:12Z"
 
 
 @_recorder.record(file_path=MOCK_SMOKE_CURRENT_PATH)
